4f5c71ae4f8550d90b9d465bc57a6a89dc2f4677
Evan Racah <eracah@lbl.gov>
2016-03-24 08:36:27 -0700

    Updated setup make dataset to work with mpi4py, root and pandas

--- extract_ibd/setup_make_dataset.sh
+++ extract_ibd/setup_make_dataset.sh
@@ -1,5 +1,8 @@
-module swap PrgEnv-intel PrgEnv-gnu
+#module swap PrgEnv-intel PrgEnv-gnu
+module unload python
+module unload h5py
 module load python/2.7-anaconda
 module load root 
-export PYTHONPATH=/global/project/projectdirs/das/wbhimji/mantissa-hep/dayabay:$PYTHONPATH 
-export PYTHONPATH=/usr/common/usg/root/5.34/gnu/lib/root/:$PYTHONPATH
+export PYTHONPATH=/global/project/projectdirs/das/racah:$PYTHONPATH 
+export PYTHONPATH=/usr/common/software/root/6.02.12/lib/root:/usr/common/software/root/6.02.12/bin:$PYTHONPATH
+
