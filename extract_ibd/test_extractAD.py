0a4b653cea551ce6e4159534493761f227bdc182
Sam <kohn@berkeley.edu>
2016-05-23 16:40:05 -0700

    Add python logging statements & remove old comments

--- extract_ibd/test_extractAD.py
+++ extract_ibd/test_extractAD.py
@@ -15,6 +15,8 @@ import pickle as pkl
 import pandas
 import h5py
 import thread
+import logging
+logging.basicConfig(level=logging.DEBUG)
 
 NFEATURES = 11 + 4*192
 
@@ -24,23 +26,20 @@ def main():
     X = pandas.read_csv(filename, delimiter='\t')
     fdict = pkl.load(open('fdict_wahid_v2.pkl', 'r')) # Contains map from (run, filenum) to full filepath.
     Nstart = int( sys.argv[1]) 
-    start = Nstart * 1  * 10**4 # 10k events per file 
-#    stop =  (Nstart + 1) * 1 * 10**4 - 1  #10
-#    start = 1
-#    stop =  2
-    N = 1 * 10**4
-#    stop = start + N 
+    N = 10000 if len(sys.argv)== 2 else int(sys.argv[2])  # 10k events per file 
+    start = Nstart * N
+    stop = start + N 
     outfile = 'ibd_v4_time_%d.h5' % start
-    #outfile = 'trial_ibd_%d.h5' % N
     data = np.zeros((N, NFEATURES), dtype='f4')
     i = -1
     for j,trigger in X.iterrows():
+        logging.debug('Start of loop')
         if (trigger['RunNo'], trigger['FileNo']) not in fdict:
-            print 'Error: Could not find run %d file %d' % (trigger['RunNo'], trigger['FileNo'])
+            logging.info('Could not find run %d file %d',trigger['RunNo'], trigger['FileNo'])
             continue
         fn = fdict[(trigger['RunNo'], trigger['FileNo'])]
         if not os.access(fn, os.R_OK):
-            print 'Error: Could not read file %s' % fn
+            logging.info('Could not read file %s', fn)
             continue
         i += 1
         if i < start:
@@ -48,10 +47,10 @@ def main():
         if i >= stop:
             break # Early
         try:
-            print fn
+            logging.debug(fn)
             rval = extract_candidate(fn, [trigger['Detector']+1, trigger['Detector']+1], [trigger['trigno_prompt'], trigger['trigno_delayed']])
         except:
-            print 'Error: Could not load trees from %s' % fn
+            logging.error('Error: Could not load trees from %s', fn)
             i -= 1
             continue
         data1, data2 = rval[0], rval[1]
@@ -65,27 +64,17 @@ def main():
         dset[0:N, ...] = data[:, 11:]
         dset2[0:N, ...] = data[:, :11]
         f.close()
-#        #h5file, node = init_hdf5(filename, ([nexamples, ninputs], [nexamples, noutputs]))
-        #fill_hdf5(h5file, data_x=X, data_y=Y, node=node, start=0)
 
 def extract_candidate(filename, detectors, triggerNumbers):   
  
     treename = '/Event/CalibReadout/CalibReadoutHeader'
     intbranches = ['nHitsAD','triggerNumber', 'detector']
     floatbranches = []
-    ivectorbranches = ["ring","column","wallNumber"] #,"wallspot"]
+    ivectorbranches = ["ring","column","wallNumber"]
     fvectorbranches = ["timeAD","chargeAD", "timePool", "chargePool", "wallSpot"]
     t1 = roottools.RootTree(filename, treename, intbranches=intbranches, floatbranches=floatbranches, ivectorbranches=ivectorbranches, fvectorbranches=fvectorbranches)
  
-    #treename = '/Event/Data/CalibStats'
-    #floatbranches = ['flasher_flag', 'MaxQ', 'Quadrant', 'time_PSD', 'time_PSD1', 'MaxQ_2inchPMT'] #, 'dtLast_AD1_ms', 'dtLast_AD2_ms', 'dtLast_AD3_ms', 'dtLast_AD4_ms'] 
-    #intbranches = ['triggerNumber']#["detector","triggerNumber"] #,"triggerType","triggerTimeSec","triggerTimeNanoSec","nHitsAD","nHitsPool"]    
-    #ivectorbranches = []
-    #fvectorbranches = []
-    #t2 = roottools.RootTree(filename, treename, intbranches=intbranches, floatbranches=floatbranches, ivectorbranches=ivectorbranches, fvectorbranches=fvectorbranches)
-    #requirements = [('detector', detector), ('triggerNumber', triggernum)]
-
-    startidx = 0 #triggerNumbers[0]
+    startidx = 0
     rval = []
     for (detector, triggerNumber) in zip(detectors, triggerNumbers): 
         idx = t1.find_trigger(detector, triggerNumber, startidx)
