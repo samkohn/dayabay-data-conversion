bbb9626571d5d4791bba24181c2eb45d45b0e3ef
Sam Kohn <kohn@berkeley.edu>
2016-08-15 11:57:02 -0700

    Update setup script

--- extract_ibd/setup_make_dataset.sh
+++ extract_ibd/setup_make_dataset.sh
@@ -1,9 +1,5 @@
-#module swap PrgEnv-intel PrgEnv-gnu
-module unload python
-module unload h5py
-module switch PrgEnv-intel PrgEnv-gnu
-module load python/2.7-anaconda
-module load root 
+source ~skohn/setup_ml_cori.sh
+module load root
 export PYTHONPATH=/global/project/projectdirs/das/racah:$PYTHONPATH 
 export PYTHONPATH=/global/homes/s/skohn/ml/dayabay-data-conversion/:$PYTHONPATH
 export PYTHONPATH=/usr/common/software/root/6.02.12/lib/root:/usr/common/software/root/6.02.12/bin:$PYTHONPATH
